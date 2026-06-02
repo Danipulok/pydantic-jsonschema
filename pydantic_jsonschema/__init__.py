0a278067effd6bb8ad4782e0b3469cc7a60eac3b
Danipulok <danipulok@gmail.com>
2026-06-02 10:37:20 +0300

    feat(init): export `Reference` in public API

--- pydantic_jsonschema/__init__.py
+++ pydantic_jsonschema/__init__.py
@@ -1,3 +1,5 @@
+"""Public API for the pydantic_jsonschema package."""
+
 from pydantic_jsonschema._version import __version__
 from pydantic_jsonschema.converters import (
     LaxSchemaConverter,
@@ -6,13 +8,14 @@ from pydantic_jsonschema.converters import (
     to_lax_model,
     to_model,
 )
-from pydantic_jsonschema.types import DataType, JsonType, Schema
+from pydantic_jsonschema.types import DataType, JsonType, Reference, Schema
 
 __all__ = [
     "DataType",
     "JsonType",
     "LaxSchemaConverter",
     "Ref",
+    "Reference",
     "Schema",
     "SchemaConverter",
     "__version__",
