7248b4ebb9e85b216eb85789aa042196215351d8
Danipulok <danipulok@gmail.com>
2026-06-23 15:14:27 +0000

    feat(exceptions): re-export exception classes from the package root

--- pydantic_jsonschema/__init__.py
+++ pydantic_jsonschema/__init__.py
@@ -3,13 +3,23 @@
 from importlib.metadata import version as _metadata_version
 
 from pydantic_jsonschema.converters import SchemaConverter, to_model
+from pydantic_jsonschema.exceptions import (
+    BasePydanticJsonSchemaError,
+    FormatValidationError,
+    SchemaConversionError,
+    SchemaReferenceError,
+)
 from pydantic_jsonschema.schema import DataType, Reference, Schema
 
 __all__ = [
+    "BasePydanticJsonSchemaError",
     "DataType",
+    "FormatValidationError",
     "Reference",
     "Schema",
+    "SchemaConversionError",
     "SchemaConverter",
+    "SchemaReferenceError",
     "__version__",
     "to_model",
 ]
